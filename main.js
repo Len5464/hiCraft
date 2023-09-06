282c7c86f88aaa9ab5f7229f35bf1774282c03a3
len5464 <zawaleng@gmail.com>
2023-09-07 01:08:51 +0800

    新增好物推薦、關注中頁面。navbar新增滾動顯示隱藏搜尋欄。調整all.scss匯入順序。新增ejs模版資料

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import "./assets/scss/all.scss";
 import * as bootstrap from "bootstrap";
+let prevScrollPos = window.pageYOffset;
+const navSearch = document.querySelector("#navSearch");
+if (navSearch) {
+  const bsSearchCollapse = new bootstrap.Collapse("#navSearch");
+  window.onscroll = function () {
+    if (navSearch.classList.contains("collapsing")) return;
+    const currentScrollPos = window.pageYOffset;
+    currentScrollPos > prevScrollPos ? bsSearchCollapse.hide() : bsSearchCollapse.show();
+    prevScrollPos = currentScrollPos;
+  };
+}
