661d99ed291fd342de910dd0ecf54cb2ac42f317
len5464 <zawaleng@gmail.com>
2023-09-11 14:38:30 +0800

    新增：搜尋結果頁面 修改：關注中標籤列、搜尋欄

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ import "./assets/scss/all.scss";
 import * as bootstrap from "bootstrap";
 let prevScrollPos = window.pageYOffset;
 const navSearch = document.querySelector("#navSearch");
+const tagListTips = document.querySelector("#tagListTips");
+if (tagListTips) {
+  const popover = new bootstrap.Popover(tagListTips, {
+    trigger: "focus",
+  });
+}
+
 if (navSearch) {
-  const bsSearchCollapse = new bootstrap.Collapse("#navSearch");
+  const bsSearchCollapse = new bootstrap.Collapse(navSearch);
   window.onscroll = function () {
     if (navSearch.classList.contains("collapsing")) return;
     const currentScrollPos = window.pageYOffset;
