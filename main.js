fd8f90ba4c37e3ae42118757c2b0ca9283a5daea
len5464 <zawaleng@gmail.com>
2023-10-18 17:24:41 +0800

    修改: 圖片磚新增按鈕加載功能 修改: 添加template-data手工藝品資料

--- main.js
+++ main.js
@@ -41,20 +41,54 @@ if (toastTrigger) {
   toastTrigger.addEventListener("submit", showToast);
 }
 galleries.forEach((gallery) => {
-  const loadGallery = createGalleryLoadHandler(gallery);
-  window.addEventListener("resize", debounce(loadGallery));
-  loadGallery();
+  const onResize = () => {
+    reloadGallery(gallery);
+  };
+  window.addEventListener("resize", debounce(onResize));
+  reloadGallery(gallery);
 });
 
 document.querySelectorAll("#nav-hot-tab , #nav-newcomer-tab , #nav-topic-tab").forEach((tab, i) => {
-  const loadGallery = createGalleryLoadHandler(galleries[i]);
-  tab.addEventListener("shown.bs.tab", loadGallery);
+  const onTabActive = () => {
+    reloadGallery(galleries[i]);
+  };
+  tab.addEventListener("shown.bs.tab", onTabActive);
+});
+document.querySelectorAll(".load-img").forEach((btn) => {
+  const gallery = btn.previousElementSibling;
+  const loadIMGs = createImgLoadHandler(gallery, 9);
+  btn.addEventListener("click", loadIMGs);
+  loadIMGs();
 });
 
-function fetchImgURLs(galleryElm) {
+function createImgLoadHandler(galleryElm, numToLoad) {
   const conditions = galleryElm.dataset.filter.split(" ");
   const items = templateData.crafts.filter((item) => item.tags.some((tag) => conditions.includes(tag)));
-  return items.map((item) => item.imgURLs[0]);
+  let urls = items.map((item) => item.imgURLs[0]);
+  let shortestIndex = 0;
+  let shortestHight = 0;
+  return () => {
+    const chunk = urls.splice(0, numToLoad);
+    if (urls.length === 0) galleryElm.nextElementSibling.textContent = "到底了";
+    if (chunk.length) {
+      console.log(`正在載入${chunk.length}個圖片`);
+      chunk.forEach((url) => {
+        const newImg = document.createElement("img");
+        newImg.src = url;
+        newImg.classList = "gallery-img w-100 d-block";
+        newImg.onload = (event) => {
+          const newItemHTML = `<a href='craft.html'>${event.target.outerHTML}</a>`;
+          galleryElm.children[shortestIndex].innerHTML += newItemHTML;
+          const colsHightArr = Array.from(galleryElm.children, (col) =>
+            col.lastChild ? col.lastChild.lastChild.offsetTop + col.lastChild.lastChild.height : 0
+          );
+          shortestHight = Math.min(...colsHightArr);
+          shortestIndex = colsHightArr.indexOf(shortestHight);
+          galleryElm.style.height = `${shortestHight}px`;
+        };
+      });
+    }
+  };
 }
 
 function createScrollToggleHandler(toggleElm) {
@@ -67,40 +101,23 @@ function createScrollToggleHandler(toggleElm) {
     prevScrollPos = currentScrollPos;
   };
 }
-
-function createGalleryLoadHandler(galleryElm) {
+function reloadGallery(galleryElm) {
   const colHTML = '<div class="col d-flex flex-column gap-6" ></div>';
-  const imgURLs = fetchImgURLs(galleryElm);
-  return () => {
-    if (window.innerWidth >= 992 && window.innerWidth < 3840) galleryElm.innerHTML = colHTML.repeat(3);
-    else if (window.innerWidth >= 576 && window.innerWidth < 992) galleryElm.innerHTML = colHTML.repeat(2);
-    else if (window.innerWidth >= 0 && window.innerWidth < 576) galleryElm.innerHTML = colHTML.repeat(1);
-    else return;
-    imgURLs.forEach((url) => {
-      const newImg = document.createElement("img");
-      newImg.src = url;
-      newImg.classList = "gallery-img w-100 d-block";
-      newImg.onload = (event) => {
-        const colsHightArr = Array.from(galleryElm.children, (col) =>
-          col.lastChild ? col.lastChild.lastChild.offsetTop + col.lastChild.lastChild.height : 0
-        );
-        const shortestIndex = colsHightArr.indexOf(Math.min(...colsHightArr));
-        const newItemHTML = `<a href='craft.html'>${event.target.outerHTML}</a>`;
-        galleryElm.children[shortestIndex].innerHTML += newItemHTML;
-      };
-    });
-  };
+  const imgNum = galleryElm.querySelectorAll(".gallery-img").length;
+  if (window.innerWidth >= 992 && window.innerWidth < 3840) galleryElm.innerHTML = colHTML.repeat(3);
+  else if (window.innerWidth >= 576 && window.innerWidth < 992) galleryElm.innerHTML = colHTML.repeat(2);
+  else if (window.innerWidth >= 0 && window.innerWidth < 576) galleryElm.innerHTML = colHTML.repeat(1);
+  else return;
+  const loadIMGs = createImgLoadHandler(galleryElm, imgNum);
+  loadIMGs();
 }
-function showToast() {
-  const toastList = document.querySelectorAll(".toast");
-  if (toastList.length) {
-    toastList.forEach((toastElm) => {
-      const toast = new bootstrap.Toast(toastElm);
-      toast.show();
-    });
-  } else {
-    console.log("找不到吐司");
-  }
+
+function showToast(event) {
+  event.preventDefault();
+  document.querySelectorAll(".toast").forEach((toastElm) => {
+    const toast = bootstrap.Toast.getOrCreateInstance(toastElm);
+    toast.show();
+  });
 }
 function throttle(fn, delay = 500) {
   let timer = null;
